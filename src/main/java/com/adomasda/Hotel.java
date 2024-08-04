bb332b0bc1c56a86148c3a1bcb16f57fe5e7d228
Adomas Daškevičiaus <adomasda@gmail.com>
2024-08-04 17:41:59 +0300

    ok, what now?

--- src/main/java/com/adomasda/Hotel.java
+++ src/main/java/com/adomasda/Hotel.java
@@ -12,20 +12,101 @@ interface WithConditioning extends CommonRoom {
     boolean isOn();
 }
 
-interface Kitchen extends CommonRoom {
+interface KitchenNamespaceConflict extends CommonRoom {
     boolean isOvenOn();
 }
 
-interface ServiceRoom extends CommonRoom {
+interface ServiceRoomNamespaceConflict extends CommonRoom {
     boolean getAccess();
 }
 
-interface Garage extends CommonRoom {
+interface GarageNamespaceConflict extends CommonRoom {
     boolean isEmpty();
 }
 
-class CherryTreeHotel implements Kitchen, ServiceRoom {
-    public boolean isOvenOn() { return false; }
-    public boolean lightIsOn() { return false; }
-    public boolean getAccess() { return false; }
+class FirstRoom implements WithBalcony {
+    boolean field = false;
+
+    public void setField(boolean set) {
+        this.field = set;
+    }
+
+    public boolean isOpen() {
+        return field;
+    }
+
+    public boolean lightIsOn() {
+        return field;
+    }
 }
+
+class SecondRoom implements WithConditioning {
+    boolean field = false;
+
+    public void setField(boolean set) {
+        this.field = set;
+    }
+
+    public boolean isOn() {
+        return field;
+    }
+
+    public boolean lightIsOn() {
+        return field;
+    }
+}
+
+class Kitchen implements KitchenNamespaceConflict {
+    boolean field = false;
+
+    public void setField(boolean set) {
+        this.field = set;
+    }
+
+    public boolean isOvenOn() {
+        return field;
+    }
+
+    public boolean lightIsOn() {
+        return field;
+    }
+}
+
+
+class ServiceRoom implements ServiceRoomNamespaceConflict {
+    boolean field = false;
+
+    public void setField(boolean set) {
+        this.field = set;
+    }
+
+    public boolean getAccess() {
+        return field;
+    }
+
+    public boolean lightIsOn() {
+        return field;
+    }
+}
+
+class Garage implements GarageNamespaceConflict {
+    boolean field = false;
+
+    public void setField(boolean set) {
+        this.field = set;
+    }
+
+    public boolean isEmpty() {
+        return field;
+    }
+
+    public boolean lightIsOn() {
+        return field;
+    }
+}
+
+//class CherryTreeHotel implements Kitchen, ServiceRoom {
+//    public boolean isOvenOn() { return false; }
+//    public boolean lightIsOn() { return false; }
+//    public boolean getAccess() { return false; }
+//}
